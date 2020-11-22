c43d157cc758539b418d8f4a2c1efda9966ace8c
Ismael Velez Recio <ismael.velez@alu.uclm.es>
2020-11-22 21:32:53 +0100

    Videos de ejemplo

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# LabMultimedia
+# Laboratorio_Multimedia
+Proyecto para el laboratorio de multimedia
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lab-multimedia';
+  
+  posicion: number = 0;
+  selectedvideo: string = "";
+  lista_videos: Array<string> = ["//vjs.zencdn.net/v/oceans.mp4", "https://cdn.videvo.net/videvo_files/video/free/2014-12/small_watermarked/Raindrops_Videvo_preview.webm"];
+
+  constructor() { 
+    this.selectedvideo = "//vjs.zencdn.net/v/oceans.mp4"
+  }
+
+  next_video() {
+    this.posicion++;
+    if (this.posicion == this.lista_videos.length){ 
+      this.posicion = 0;
+    }
+    console.log(this.posicion)
+    this.selectedvideo = this.lista_videos[this.posicion];
+    console.log(this.selectedvideo)
+  }
+
 }
